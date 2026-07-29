90db13756da1a7156eb972bb3a4eed6d8a12ed3f
SHIFAH MEDICAL TRAINING COLLEGE <shifahmarketing@gmail.com>
2026-07-29 14:46:05 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -208,7 +208,7 @@ Welcome to Shifah Medical Training College—where excellence is nurtured, innov
           <div className="absolute bottom-8 left-8">
 
             <p className="text-xl font-bold text-white">
-              Mr. Jactone M. Njoroge,PHN
+              Mr. Jactone A. Ocharo,PHN
             </p>
 
             <p className="text-sm uppercase tracking-widest text-brand-gold">
